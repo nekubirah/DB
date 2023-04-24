09dbf7c05dd04f62e225b679a3022d3f3ceba915
Nekubirah <120730233+nekubirah@users.noreply.github.com>
2023-04-24 19:38:02 +0200

    Update timetable.php
    Formatierung der End-Daten ins Lesbare

--- db/timetable.php
+++ db/timetable.php
@@ -1,7 +1,6 @@
 <?php
 
-
-
+//Curl
 $ch = curl_init("https://apis.deutschebahn.com/db-api-marketplace/apis/station-data/v2/stations");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, array(
@@ -21,43 +20,41 @@ $response = json_decode($response, true);
 $m=0;
  foreach ($response['result'] as $h){
         $bahnhof_name[$m] = $response['result'][$m]['name'];
-
-        $bahnhof_name[$m] = str_replace(" ( ","(",$bahnhof_name[$m]);
-        $bahnhof_name[$m] = str_replace(" (b ","-(bei ",$bahnhof_name[$m]);
-        $bahnhof_name[$m] = str_replace(" ) ",")",$bahnhof_name[$m]);
-        $bahnhof_name[$m] = str_replace(" )",")",$bahnhof_name[$m]);
-        $bahnhof_name[$m] = str_replace(" ","-",$bahnhof_name[$m]);
-
         $bahnhof_number[$m] = $response['result'][$m]['number'];
         $bahnhof_eva[$m] = $response['result'][$m]['evaNumbers'];
        // $bahnhof_step[$m] = $response['result'][$m]['hasSteplessAccess'];
         $m++;
  }
 
- echo $bahnhof_name[0] ,"</br>";
- echo $bahnhof_number[0] ,"</br>";
-
-
-$evanr = explode(",",json_encode($bahnhof_eva[0]));
-$evanr_true = explode(":",$evanr[0]);
-//echo $evanr_true[1], "</br>";
+ 
     
+// Bahnhof Daten Raussuchen
 $i=0;
 foreach($bahnhof_name as $name){
     if(json_encode($name) == json_encode($_POST["vonliste"])){
-        echo $bahnhof_name[$i] ,"</br>";
-        echo $bahnhof_number[$i] ,"</br>";
-
+        //echo $bahnhof_name[$i] ,"</br>";
+        //echo $bahnhof_number[$i] ,"</br>";
+        
         $evanr = explode(",",json_encode($bahnhof_eva[$i]));
         $evanr_true = explode(":",$evanr[0]);
-        echo $evanr_true[1], "</br>";
+        //echo $evanr_true[1], "</br>";
     }
     $i++;
 }
 
-$curl2 = curl_init();
+//datum Übergabe
+$date=$_POST["date"];
+$date = str_replace("-","",$date);
+$date = substr($date, 2);
 
-$link ="https://apis.deutschebahn.com/db-api-marketplace/apis/timetables/v1/plan/".$evanr_true[1]."/230310/11";
+//time Übergabe
+$time = $_POST["time"];
+$time = explode(":",$time);
+
+
+//Abfrage der Time Tables
+$curl2 = curl_init();
+$link ="https://apis.deutschebahn.com/db-api-marketplace/apis/timetables/v1/plan/".$evanr_true[1]."/".$date."/".$time[0];
 curl_setopt_array($curl2, [
   CURLOPT_URL => $link,
   CURLOPT_RETURNTRANSFER => true,
@@ -74,16 +71,43 @@ curl_setopt_array($curl2, [
 ]);
 
 $response2 = curl_exec($curl2);
-
 curl_close($curl2);
- echo $link,"<br>";
 
-    echo "1";
-  echo $response2;
-  echo "2";
+
+//link zum Prüfen
+echo $link,"<br><br>";
+
+//test ob Daten übergeben wurden
+
   $str = htmlentities($response2 , ENT_XML1);
+  if ($str == ""){ echo "404 - No Data Found";}
   echo $str;
   
+echo "<br><br>";
+
+
+//Daten formatieren
+
+$xml = simplexml_load_string($response2);
+
+echo "Station: " . $xml['station'] . "<br><br>";
+
+foreach ($xml->s as $s) {
+    echo "ID: " . $s['id'] . "<br>";
+    echo "Zug: " . $s->tl['c'] . " ";
+    echo $s->dp['l'] . "<br>";
+
+    echo "Abfahrt: " .$s->dp['pt']. "<br>";
+    echo "Abfahrts Gleis: " . $s->dp['pp'] . "<br>";
+    echo "Abfahrt Weg: " . $s->dp['ppth'] . "<br> <br>";
+
+    if (isset($s->ar)) {
+        echo "Ankunft: " .$s->ar['pt']. "<br>";
+        echo "Ankunfts Gleis: " . $s->ar['pp'] . "<br>";
+        echo "Ankunft Weg: " . $s->ar['ppth'] . "<br> <br>";
+    }
+}
+
 
 ?>
 
